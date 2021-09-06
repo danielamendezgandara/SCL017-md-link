38659d8e2405d0c4441cf4030ef1cbda732b9669
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-09-05 23:50:16 -0300

    file is renamed and progress.argv is added to capture the file path.

--- modules/joinRoutes-module.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-const route = path.join('../helpers','js','test.js');
-
-console.log(route);
---
+++ modules/joinRoutes.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const folder= process.argv[2];
+
+const extension = process.argv[3];
+
+const file = process.argv[4];
+
+const route = path.join(folder,extension,file);
+
+console.log(route);
