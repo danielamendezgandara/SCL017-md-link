12b73dbb53a7eeacd7d5da04f8da641232518fb8
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-09-05 23:45:58 -0300

    file is renamed and progress.argv is added to capture the file path.

--- modules/getExtensionFile-module.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-const extension=path.extname('../files_txt/file1.txt');
-
-console.log(extension);
---
+++ modules/getExtensionFile.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const file = process.argv[2];
+
+const extension=path.extname(file);
+
+console.log(extension);
