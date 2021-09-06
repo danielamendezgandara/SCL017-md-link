bc3cf09456dae130e2a75d5b1145c57d7127acd9
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-09-05 23:39:29 -0300

    file is renamed and progress.argv is added to capture the file path.

--- modules/read.js
+++ modules/read.js
@@ -1,6 +1,8 @@
 const fs=require('fs');
 
-fs.readFile('../files_txt/file1.txt','utf-8',(err,data)=>{
+const file = process.argv[2];
+
+fs.readFile(file,'utf-8',(err,data)=>{
     if(err){
         console.log('error:',err);
     }else{
